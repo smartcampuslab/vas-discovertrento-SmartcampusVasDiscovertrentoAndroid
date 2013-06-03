6fdb10f3a9e2918799e02e1b0f7839e0f0508923
chin8 <m.chini@trentorise.eu>
2013-06-03 15:02:04 +0200

    add params for smartcampus/vivitrento

---
+++ assets/params_dt.js
@@ -0,0 +1,51 @@
+{
+  "title": "ViviTrentino",
+   "poi_categories":[
+
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        10,
+        11,
+        13
+      ],
+   "events_categories":[
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        9
+        
+      ],
+   "story_categories":[
+        1,
+        2,
+        3,
+        4,
+        5
+      ],
+    "exclude":{
+    	source:[
+    	        "smartplanner-transitstops",
+    	        "TrentinoFamiglia"
+    	        ],
+    	type:"Comune"
+   },
+   "app_token":"familytrento",
+   "center_map":[
+                     46069672.7540531,
+                     11121270.0605392
+                     ],
+   "zoom_map":15
+  
+  
+}
